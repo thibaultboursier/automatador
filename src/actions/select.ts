193e415ae696adb16237ac6d5837308056b9629c
thibaultboursier <boursier.thibault@gmail.com>
2019-04-30 22:36:09 +0200

    feat(select): check if element has select tag

--- src/actions/select.ts
+++ src/actions/select.ts
@@ -4,6 +4,10 @@ import { hover } from './hover';
 interface SelectOptions{}
 
 export const select = async (element: HTMLSelectElement, label: string, options: SelectOptions = {}) => {
+  if (element.tagName.toLowerCase() !== 'select') {
+    throw new Error('A select element must be provided');
+  }
+
   await hover(element);
 
   const selectedIndex = Array.from(element.options).findIndex(option => option.label === label);
