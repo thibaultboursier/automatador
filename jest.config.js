a068cdde96baccdb3dcf85abc457d458b80f03db
thibaultboursier <boursier.thibault@gmail.com>
2019-05-01 21:02:47 +0200

    chore(npm): add jest dependencies and configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleNameMapper: {
+    "\\.(png)$":"<rootDir>/__mocks__/pngMock.js",
+  }
+};
