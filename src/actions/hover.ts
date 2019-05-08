2e7d4282e138af666821036286a53e3efadc813a
thibaultboursier <boursier.thibault@gmail.com>
2019-05-08 20:46:28 +0200

    fix(hover): resolve hover promise on the next event queue

--- src/actions/hover.ts
+++ src/actions/hover.ts
@@ -10,7 +10,10 @@ export const hover = <T extends HTMLElement>(element: T): Promise<void> => {
       return reject('Cursor element was not found');
     }
 
-    cursor.addEventListener('transitionend', () => resolve());
+    cursor.addEventListener('transitionend', function callback() {
+      cursor.removeEventListener('transitionend', callback);
+      setTimeout(resolve, 0);
+    });
 
     cursor.style.left = `${left + offsetWidth / 2}px`;
     cursor.style.top = `${top + offsetHeight / 2}px`;
